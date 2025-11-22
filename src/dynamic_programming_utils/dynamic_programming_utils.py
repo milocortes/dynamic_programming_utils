2d1c452e716e6a1a86dd46a8f0826495efc5e71c
Hermilo Cortes <hermilocg@hotmail.com>
2025-11-21 18:22:12 -0600

    first commit

---
+++ src/dynamic_programming_utils/dynamic_programming_utils.py
@@ -0,0 +1,36 @@
+import numpy as np 
+import math 
+
+def rouwenhorst(
+    N : int, 
+    ρ : float,
+    σ : float,
+    μ : float = 0.0):
+    """
+    #=#############################################################################
+    # SUBROUTINE discretize_AR
+    #
+    # Discretizes an AR(1) process of the form z_j = \rho*z_{j-1} + eps using
+    #     the Rouwenhorst method.
+    #
+    # REFERENCE: Kopecky, K.A., Suen, R.M.H., Finite state Markov-chain 
+    #            approximations to highly persistent processes, Review of Economic
+    #            Dynamics, Vol. 13, No. 3, 2010, 701-714.
+    =##############################################################################
+    """
+
+    σ_y = σ / math.sqrt(1-ρ^2)
+    p  = (1+ρ)/2
+    ψ = math.sqrt(N-1) * σ_y
+    m = μ / (1 - ρ)
+
+
+
+def  _rouwenhorst(p : float, 
+                  q : float, 
+                  m : float, 
+                  Δ : float, 
+                  n : int)
+
+    if n == 2:
+        return [m-Δ, m+Δ],  [p 1-p; 1-q q]
